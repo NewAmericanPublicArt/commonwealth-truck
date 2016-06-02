cd2c0edb5a758604b772fb13f7c6399576bafd50
Brandon Stafford <brandon@pingswept.org>
2016-06-02 15:43:37 -0400

    Make LED color track potentiometer using FastLED library

--- commonwealth-truck.ino
+++ commonwealth-truck.ino
@@ -17,19 +17,40 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-void setup() {
-  Serial.begin(115200);
-  pinMode(13, OUTPUT);
+#include <FastLED.h>
+
+#define REDPIN   4
+#define GREENPIN 3
+#define BLUEPIN  5
+
+void showAnalogRGB( const CRGB& rgb) {
+  analogWrite(REDPIN,   255-rgb.r );
+  analogWrite(GREENPIN, 255-rgb.g );
+  analogWrite(BLUEPIN,  255-rgb.b );
 }
 
-// the loop routine runs over and over again forever:
+void colorBars() {
+  showAnalogRGB( CRGB::Red );   delay(3000);
+  showAnalogRGB( CRGB::Green ); delay(3000);
+  showAnalogRGB( CRGB::Blue );  delay(3000);
+  showAnalogRGB( CRGB::Black ); delay(1000);
+}
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(13, OUTPUT);
+    pinMode(REDPIN, OUTPUT);
+    pinMode(GREENPIN, OUTPUT);
+    pinMode(BLUEPIN, OUTPUT);
+    // Flash the "hello" color sequence: R, G, B, black.
+    colorBars();
+}
+
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // print out the value you read:
-  Serial.println(sensorValue);
-  digitalWrite(13, HIGH);
-  delay(1 * sensorValue);
-  digitalWrite(13, LOW);
-  delay(1 * sensorValue);
+    uint8_t hue;
+    int sensorValue = analogRead(A0);
+    hue = map(sensorValue, 0, 1023, 0, 255);
+    showAnalogRGB(CHSV( hue, 255, 255));
+    Serial.println(hue);
+    delay(20);
 }
